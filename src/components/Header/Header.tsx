f510b1ba57c79300d4ec029ce8ac5affb978400b
Никита Швец <dustikappple@icloud.com>
2023-08-22 04:28:19 +0700

    header implementation without menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,21 @@
+import './Header.scss';
+import { useState } from 'react';
+import burgerMenu from './assets/burgerMenu.svg';
+import shopBag from './assets/shopBag.svg';
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="left-buttons">
+        <img src={burgerMenu} alt="burger-menu" className="mobile-only" />
+        <div className="left-button desktop-only">Shop</div>
+        <div className="left-button desktop-only">Contact</div>
+      </div>
+      <div className="right-buttons">
+        <img src={shopBag} alt="shop-bag" className="mobile-only" />
+        <div className="sign-in-button desktop-only">Sign In</div>
+        <div className="cart-button desktop-only">Cart</div>
+      </div>
+    </header>
+  );
 };
